bf133781ca32b2bfdbcc9211e2562ac84fed5cf7
digi <95752749+digi9999@users.noreply.github.com>
2022-07-12 00:24:23 +0530

    Update AbstractView.js

--- spa/public/views/AbstractView.js
+++ spa/public/views/AbstractView.js
@@ -3,7 +3,6 @@ export default class {
     // this.mobile = navigator.userAgentData.mobile;
     this.mobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   }
-  
   setTitle(title) {
     document.title = title;
   }
