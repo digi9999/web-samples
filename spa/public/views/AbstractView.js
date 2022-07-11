93e6d7d352042feec3af58626cfefde154542f4f
digi <95752749+digi9999@users.noreply.github.com>
2022-07-12 00:23:42 +0530

    Create AbstractView.js

---
+++ spa/public/views/AbstractView.js
@@ -0,0 +1,13 @@
+export default class {
+  constructor() {
+    // this.mobile = navigator.userAgentData.mobile;
+    this.mobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  }
+  
+  setTitle(title) {
+    document.title = title;
+  }
+  async getHtml() {
+    return "";  
+  }
+}
