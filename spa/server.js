e02e1f3bd52731be61b5ff6aa12ea74e901172c2
digi <95752749+digi9999@users.noreply.github.com>
2022-07-12 00:20:02 +0530

    Create server.js

---
+++ spa/server.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const fastify = require("fastify")({logger: false});
+fastify.register(require("fastify-static"), {root: path.join(__dirname, "public"),prefix: "/"});
+
+
+fastify.get("*" , async (req, reply) => {  
+  const stream = fs.readFileSync(__dirname+'/src/pages/index.html');
+  reply.type('text/html').send(stream)
+});
+
+
+fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Your app is listening on ${address}`);
+  fastify.log.info(`server listening on ${address}`);
+});
