c075a42b06b3840e4f9dac840e0ec41da4eeae92
digi <95752749+digi9999@users.noreply.github.com>
2022-07-12 00:27:43 +0530

    Create AboutView.js

---
+++ spa/public/views/AboutView.js
@@ -0,0 +1,12 @@
+import AbstractView from "./AbstractView.js"
+
+export default class extends AbstractView {
+  constructor() {
+    super();
+    this.setTitle("About");
+  }
+
+  async getHtml(){
+    return ``;
+  }
+}
